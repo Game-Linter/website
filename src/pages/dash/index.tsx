395fe8012bd5f82977b5603b35cd6c1c63ad360d
Mohamed Belkamel <belkamelmohamed@gmail.com>
2020-12-13 16:16:35 +0100

    removed with crdentials from currentuser

--- src/pages/dash/index.tsx
+++ src/pages/dash/index.tsx
@@ -14,7 +14,7 @@ export const getServerSideProps = async ({ req }) => {
 		const { currentUser } = await axios
 			.get('https://api.game-linter.com/api/v1/currentuser')
 			.then((res) => {
-				console.log(res);
+				// console.log(res);
 				return res.data;
 			});
 		let isLogged = currentUser !== null;
