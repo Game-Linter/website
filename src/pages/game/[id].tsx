ed156d36b492edea9646ac01c38c756a328205c2
Mohamed Belkamel <belkamelmohamed@gmail.com>
2021-04-25 21:43:50 +0000

    removed font force

--- src/pages/game/[id].tsx
+++ src/pages/game/[id].tsx
@@ -473,7 +473,7 @@ export default ({
 														<span className="flex rounded-full bg-green-500 uppercase px-2 py-1 text-xs font-bold mr-3">
 															{index + 1}
 														</span>
-														<span className="font-semibold mr-2 text-left flex-auto">
+														<span className="mr-2 text-left flex-auto">
 															<ReactMarkdown>{value.content}</ReactMarkdown>
 														</span>
 													</div>
