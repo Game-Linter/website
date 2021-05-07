4f48db756fa99f49e278e5458ba1a6eb35d4e893
Mohamed Belkamel <belkamelmohamed@gmail.com>
2021-05-07 01:03:17 +0000

    config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 module.exports = {
-	assetPrefix: 'https://cdn.game-linter.com/media',
-};
-
-module.exports = {
 	images: {
 		domains: ['cdn.game-linter.com'],
 	},
-};
-
-module.exports = {
-	poweredByHeader: false,
 };
